f9fb5d5416d00f31647daba011bf0ac835a969a3
Denis Ivanchenko <sicknature24@yandex.com>
2023-02-25 15:54:30 +0300

    feat:add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.value = Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        return this.max = this.value;
     }
 
     greater() {
-
+        return this.min = this.value;
     }
 }
 
